74624cd833d470bad5b11ca5626f708e4f3ecfa2
trashgenerator <destinybox@rambler.ru>
2018-08-20 11:51:09 +0300

    Create custom HTTP client and sends 'listall' && just print all results

--- cmd/view/main.go
+++ cmd/view/main.go
@@ -1,10 +1,37 @@
 package main
 
 import (
-    "fmt"
-    l "dcore/logic"
+    "log"
+    "time"
+    "net/http"
+    "io/ioutil"
+)
+
+const (
+    TimeoutRequest = time.Second * 11
+    SignalServerRequest = "http://127.0.0.1/listall"
 )
 
 func main() {
-    fmt.Printf("VIEW %s", l.ThePhrase())
+    viewClient := &http.Client{
+        Timeout   : TimeoutRequest,
+        Transport : &http.Transport {
+            DisableKeepAlives   : true,
+            DisableCompression  : false,
+            TLSHandshakeTimeout : TimeoutRequest}}
+    resp, err := viewClient.Get(SignalServerRequest)
+    if resp != nil {
+        defer resp.Body.Close()
+    }
+    if err != nil {
+        log.Fatal(err)
+    }
+    
+    bodyBytes, err := ioutil.ReadAll(resp.Body)
+    if err != nil {
+        log.Fatal(err)
+    }
+    
+    body := string(bodyBytes)
+    log.Printf("Request from SignalServer:\n[%s]\n", body)
 }
