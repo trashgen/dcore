d594b606eac8189283d4185a2f61ff737cbff9de
trashgenerator <destinybox@rambler.ru>
2018-09-07 08:36:35 +0300

    hotfix blacklist sql dcript

---
+++ scripts/blacklist.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE IF NOT EXISTS blacklist (
+	id bigserial NOT NULL,
+	ip varchar(45) NOT NULL,
+	created timestamp default now()
+)
+WITH (OIDS=false);
+
+CREATE UNIQUE INDEX IF NOT EXISTS blacklist_ip_idx ON blacklist (ip);
